497ff3db56ed9446b73fef6c68c06911e25e8451
Cody_Jones <cbjones93@gmail.com>
2021-04-07 11:35:01 -0500

    patch fix

--- src/components/Locations/LocationList.js
+++ src/components/Locations/LocationList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getAllEmployees } from '../../modules/EmployeeManager';
 import { deleteLocation, getAllLocations } from '../../modules/LocationManager';
-import { LocationCard, locationCard } from './LocationCard';
+import { LocationCard} from './LocationCard';
 
 export const LocationList = () => {
     const handleDeleteLocation = id => {

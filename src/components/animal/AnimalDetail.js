3f89bbc17f859e42421901b080cf12638574e186
Cody_Jones <cbjones93@gmail.com>
2021-04-07 15:22:07 -0500

    discharge pets and close locations functionality added to details

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { getAnimalById } from '../../modules/AnimalManager';
+import { deleteAnimal, getAnimalById } from '../../modules/AnimalManager';
 import './AnimalDetail.css';
 import { useParams, useHistory } from "react-router-dom"
 
 export const AnimalDetail = () => {
+  const handleDelete = () => {
+    setIsLoading(true);
+    deleteAnimal(animalId).then(()=>
+    history.push("/animals"))
+  }
   const [animal, setAnimal] = useState({ name: "", breed: "" });
-
+const [isLoading, setIsLoading]= useState(true);
   const {animalId} = useParams();
   const history = useHistory();
 
@@ -20,6 +25,7 @@ export const AnimalDetail = () => {
           location:animal.location,
           customer:animal.customer
         });
+        setIsLoading(false);
       });
   }, [animalId]);
 
@@ -30,6 +36,9 @@ export const AnimalDetail = () => {
       {/* What's up with the question mark???? See below.*/}
       <div className="animal__location">Location: {animal.location?.name}</div>
       <div className="animal__owner">Customer: {animal.customer?.name}</div>
+      <button type="button" disabled={isLoading} onClick={handleDelete}>
+          Discharge
+        </button>
     </section>
   );
 }

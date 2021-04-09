2ac9b66ad2637d55ad86043ba14521ccd4efe7e0
Cody_Jones <cbjones93@gmail.com>
2021-04-09 13:17:37 -0500

    spotlight animal

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AnimalSpotlight } from "./components/animal/AnimalSpotlight"
+import { getRandomId } from "./modules/AnimalManager"
 import { PropsAndState } from '../src/components/PropsAndState'
-import {getAllData} from './modules/DataManager'
+import { getAllData } from './modules/DataManager'
 
-export const Home = () => (
-    <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
+export const Home = () => {
+    const [spotlightId, setSpotlightId] = useState(0);
+    const refreshSpotlightAnimal = () => {
+        getRandomId().then(setSpotlightId);
+    };
 
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-        <PropsAndState yourName={"Cody"} />
-    </>
-)
+    useEffect(() => {
+        refreshSpotlightAnimal();
+    }, []);
+    return (
+        <>
+            <h2>Nashville Kennels</h2>
+            <small>Loving care when you're not there.</small>
+
+            <address>
+                <div>Visit Us at the Nashville North Location</div>
+                <div>500 Puppy Way</div>
+            </address>
+            <h1>Animal Spotlight</h1>
+            <button onClick = {refreshSpotlightAnimal}>Reload &#x27f3;</button>
+            {
+                spotlightId && <AnimalSpotlight animalId={spotlightId} />
+            }
+            <PropsAndState yourName={"Cody"} />
+        </>
+    );
+}
 getAllData()
